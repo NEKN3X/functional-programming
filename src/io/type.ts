d792f466225e714fba778ab2bb8885d50ef4878b
nekn3x <nekn3x@gmail.com>
2023-09-08 01:37:30 +0900

    feat: 関数シグネチャの定義

--- src/io/type.ts
+++ src/io/type.ts
@@ -1,10 +1,25 @@
 import { type IO } from "fp-ts/IO"
 
 import type Artist from "~/model/Artist"
+import Attraction from "~/model/Attraction"
 
 import { type LocationId } from "~/model/Location"
+import Movie from "~/model/Movie"
 
 export type findArtistsFromLocation = (
   locationId: LocationId,
   limit: number
 ) => IO<ReadonlyArray<Artist>>
+
+export type findMoviesAboutLocation = (
+  locationId: LocationId,
+  limit: number
+) => IO<ReadonlyArray<Movie>>
+
+export type AttractionOrdering = "ByName" | "ByLocationPopulation"
+
+export type findAttractions = (
+  name: string,
+  ordering: AttractionOrdering,
+  limit: number
+) => IO<ReadonlyArray<Attraction>>
